58750eed9c0f90e0aefad20ddfbd7f44c175224d
HartFinne <167497706+HartFinne@users.noreply.github.com>
2025-12-12 16:33:42 +0800

    adding the /api/auth in base url

--- frontend/src/api/authAPI.js
+++ frontend/src/api/authAPI.js
@@ -4,16 +4,16 @@ import axios from 'axios'
 // const API_BASE = 'http://localhost:5000/api'
 
 // production
-const API_BASE = 'https://mini-hcm-tau.vercel.app'
+const API_BASE = 'https://mini-hcm-tau.vercel.app/api/auth'
 
 export const loginUser = async (email, password) => {
-  const res = await axios.post(`${API_BASE}api/auth/login`, { email, password });
+  const res = await axios.post(`${API_BASE}/login`, { email, password });
   return res.data // { token, role }
 };
 
 // Register function
 export const registerUser = async ({ name, email, password, timezone }) => {
-  const res = await axios.post(`${API_BASE}api/auth/register`, {
+  const res = await axios.post(`${API_BASE}/register`, {
     name,
     email,
     password,
